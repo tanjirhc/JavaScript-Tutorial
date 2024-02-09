308db6e650a2e50e454f3bd9ff3c8f66fcd7692b
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-02-09 11:29:41 +0600

    commit clicking on button 1 message

---
+++ event.js
@@ -0,0 +1,12 @@
+var myVar = document.querySelector("#paraId");
+function myMessage1(){
+    
+    myVar.innerHTML = "You have clicked on button1";
+
+}
+
+function myMessage2(){
+    
+    myVar.innerHTML = "You have clicked on button2";
+}
+
