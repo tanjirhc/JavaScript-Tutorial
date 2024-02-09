3052f96c82481d126343d481e1436ec3e76bd315
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-02-09 10:54:41 +0600

    all javascript

---
+++ backup.js
@@ -0,0 +1,6 @@
+var display1 = document.getElementById("tanjir");
+display1.innerHTML = "Dhaka, Bangladesh";
+
+var display2 = document.getElementById("dhaka");
+display2.innerHTML = "Signboard, Link Road";
+document.getElementsByTagName("h2")[0].innerHTML = "New tag";
--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-var num = 2.5687;
 
 
-console.log(typeof(Number("12")));
-console.log(typeof(Number("12.5")));
-console.log(Number(false));
