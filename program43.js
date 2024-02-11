8885ab108dc3bebc52616e94fb6db42c9cfd76e0
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-02-11 10:58:59 +0600

    button clicking event

---
+++ program43.js
@@ -0,0 +1,17 @@
+document.querySelectorAll(".myButton")[0].addEventListener("click", function() {
+
+    var text = this.innerHTML;
+    document.querySelector("h1").innerHTML =text + " "+" is clicked";
+});
+
+document.querySelectorAll(".myButton")[1].addEventListener("click", function() {
+
+    var text = this.innerHTML;
+    document.querySelector("h1").innerHTML =text + " "+" is clicked";
+});
+
+document.querySelectorAll(".myButton")[2].addEventListener("click", function() {
+
+    var text = this.innerHTML;
+    document.querySelector("h1").innerHTML =text + " "+" is clicked";
+});
