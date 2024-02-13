7e7f01514fa0396ac175de6cbff495e80145cd66
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-02-13 11:41:24 +0600

    checkbox handling by event listener

--- program47.js
+++ program47.js
@@ -1,6 +1,25 @@
-const varName = document.querySelector("input");
-varName.addEventListener("change", changedHandler)
+console.clear();
+// const varName = document.querySelector("input[name=name]");
+// varName.addEventListener("change", changedHandler);
 
-function changedHandler (e) {
-    console.log(e.type);
-}
+// function changedHandler (e) {
+//     // console.log(e.type);
+//     // console.log(e.target);
+//     // console.log(e.target.className);
+//     // console.log(e.target.id);
+//     console.log(e.target.value);
+// }
+
+
+const programs = document.querySelectorAll('input[name=program]');
+console.log(programs);
+
+Array.from(programs).map((program) => {
+    program.addEventListener("change", programHandler)
+} );
+
+function programHandler(e){
+    if(e.target.checked){
+        console.log(e.target.value);
+    }
+};
