2ca88372a3d31fcd098975b0df58671e1164d845
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-02-13 11:52:42 +0600

    select tag with event listener

--- program47.js
+++ program47.js
@@ -11,15 +11,23 @@ console.clear();
 // }
 
 
-const programs = document.querySelectorAll('input[name=program]');
-console.log(programs);
+// const programs = document.querySelectorAll('input[name=program]');
+// console.log(programs);
 
-Array.from(programs).map((program) => {
-    program.addEventListener("change", programHandler)
-} );
+// Array.from(programs).map((program) => {
+//     program.addEventListener("change", programHandler)
+// } );
 
-function programHandler(e){
-    if(e.target.checked){
-        console.log(e.target.value);
-    }
+// function programHandler(e){
+//     if(e.target.checked){
+//         console.log(e.target.value);
+//     }
+// };
+
+const department = document.querySelector('#department');
+console.log(department);
+
+department.addEventListener('change', handleDepartment);
+function handleDepartment(e) {
+    console.log(e.target.value);
 };
